a5fd5164e5913bb7c896da5fa791bc304d500861
JoacoSM18 <joako.sm.18@gmail.com>
2025-09-05 12:34:42 -0300

    Agregar archivos de proyecto.

---
+++ prySilvaMenendez_RegistroContacto_05.09/frmRegistro_Contactos.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace prySilvaMenendez_RegistroContacto_05._09
+{
+    partial class frmRegistro_Contactos
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblContacto = new System.Windows.Forms.Label();
+            this.lblTelefono = new System.Windows.Forms.Label();
+            this.lstDatos = new System.Windows.Forms.ListBox();
+            this.btnGrabar = new System.Windows.Forms.Button();
+            this.txtTelefono = new System.Windows.Forms.MaskedTextBox();
+            this.txtContacto = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblContacto
+            // 
+            this.lblContacto.AutoSize = true;
+            this.lblContacto.Font = new System.Drawing.Font("Verdana", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblContacto.Location = new System.Drawing.Point(107, 111);
+            this.lblContacto.Name = "lblContacto";
+            this.lblContacto.Size = new System.Drawing.Size(106, 23);
+            this.lblContacto.TabIndex = 1;
+            this.lblContacto.Text = "Contacto";
+            this.lblContacto.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblTelefono
+            // 
+            this.lblTelefono.AutoSize = true;
+            this.lblTelefono.Font = new System.Drawing.Font("Verdana", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTelefono.Location = new System.Drawing.Point(107, 185);
+            this.lblTelefono.Name = "lblTelefono";
+            this.lblTelefono.Size = new System.Drawing.Size(104, 23);
+            this.lblTelefono.TabIndex = 1;
+            this.lblTelefono.Text = "Telefono";
+            // 
+            // lstDatos
+            // 
+            this.lstDatos.FormattingEnabled = true;
+            this.lstDatos.Location = new System.Drawing.Point(74, 300);
+            this.lstDatos.Name = "lstDatos";
+            this.lstDatos.Size = new System.Drawing.Size(120, 95);
+            this.lstDatos.TabIndex = 2;
+            // 
+            // btnGrabar
+            // 
+            this.btnGrabar.Font = new System.Drawing.Font("MS Reference Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGrabar.Location = new System.Drawing.Point(309, 322);
+            this.btnGrabar.Name = "btnGrabar";
+            this.btnGrabar.Size = new System.Drawing.Size(132, 41);
+            this.btnGrabar.TabIndex = 3;
+            this.btnGrabar.Text = "GRABAR";
+            this.btnGrabar.UseVisualStyleBackColor = true;
+            // 
+            // txtTelefono
+            // 
+            this.txtTelefono.Location = new System.Drawing.Point(292, 188);
+            this.txtTelefono.Mask = "9999999999999999999";
+            this.txtTelefono.Name = "txtTelefono";
+            this.txtTelefono.Size = new System.Drawing.Size(149, 20);
+            this.txtTelefono.TabIndex = 4;
+            this.txtTelefono.ValidatingType = typeof(int);
+            // 
+            // txtContacto
+            // 
+            this.txtContacto.Location = new System.Drawing.Point(292, 114);
+            this.txtContacto.Name = "txtContacto";
+            this.txtContacto.Size = new System.Drawing.Size(149, 20);
+            this.txtContacto.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(154, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 5;
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Font = new System.Drawing.Font("Verdana", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl1.Location = new System.Drawing.Point(106, 20);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(307, 29);
+            this.lbl1.TabIndex = 6;
+            this.lbl1.Text = "Registro de Contactos";
+            // 
+            // frmRegistro_Contactos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lbl1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtContacto);
+            this.Controls.Add(this.txtTelefono);
+            this.Controls.Add(this.btnGrabar);
+            this.Controls.Add(this.lstDatos);
+            this.Controls.Add(this.lblTelefono);
+            this.Controls.Add(this.lblContacto);
+            this.Name = "frmRegistro_Contactos";
+            this.Text = "Registro de Contactos";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblContacto;
+        private System.Windows.Forms.Label lblTelefono;
+        private System.Windows.Forms.ListBox lstDatos;
+        private System.Windows.Forms.Button btnGrabar;
+        private System.Windows.Forms.MaskedTextBox txtTelefono;
+        private System.Windows.Forms.TextBox txtContacto;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lbl1;
+    }
+}
+
