720f1da8b8c41fbfd73af5128dcb01f9e72da51c
JoacoSM18 <joako.sm.18@gmail.com>
2025-09-05 12:43:58 -0300

    algo

--- prySilvaMenendez_RegistroContacto_05.09/frmRegistro_Contactos.Designer.cs
+++ prySilvaMenendez_RegistroContacto_05.09/frmRegistro_Contactos.Designer.cs
@@ -118,7 +118,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.BlanchedAlmond;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(484, 411);
             this.Controls.Add(this.lbl1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.txtContacto);
